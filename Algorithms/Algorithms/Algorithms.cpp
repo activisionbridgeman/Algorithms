6cf664412bf2061283123ca195263e45647b220a
activisionbridgeman <andrew.bridgeman@activision.com>
2022-08-02 10:36:11 -0500

    added dijkstra's algorithm

--- Algorithms/Algorithms/Algorithms.cpp
+++ Algorithms/Algorithms/Algorithms.cpp
@@ -1,4 +1,6 @@
 // Algorithms.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Created bubble sort algorithm and Dijkstra's algorithm
+// Dijkstra's algorithm was pulled and modified from https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
 //
 
 #include <iostream>
@@ -6,6 +8,9 @@
 #include <chrono>
 #include <tuple>
 using namespace std::chrono;
+
+// Number of vertices in graph
+const int V = 9;
 
 // Simple bubble sort algorithm for a vector of ints (returns sorted vector in ascending order)
 std::chrono::nanoseconds bubbleSort(std::vector<int>& arrayInput)
@@ -32,6 +37,70 @@ std::chrono::nanoseconds bubbleSort(std::vector<int>& arrayInput)
     return duration;
 }
 
+// A utility function to find the vertex with minimum distance value, from
+// the set of vertices not yet included in shortest path tree
+int minDistance(int dist[], bool sptSet[])
+{
+    // Initialize min value
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < V; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
+
+    return min_index;
+}
+
+// A utility function to print the constructed distance array
+void printSolution(int dist[], int src)
+{
+    std::cout << "Vertex \t Distance from Source " << src << '\n';
+    for (int i = 0; i < V; i++)
+        std::cout << i << " \t\t" << dist[i] << '\n';
+    std::cout << "\n";
+}
+
+// Function that implements Dijkstra's single source shortest path algorithm
+// for a graph represented using adjacency matrix representation
+void dijkstra(int graph[V][V], int src)
+{
+    int dist[V]; // The output array.  dist[i] will hold the shortest
+    // distance from src to i
+
+    bool sptSet[V]; // sptSet[i] will be true if vertex i is included in shortest
+    // path tree or shortest distance from src to i is finalized
+
+    // Initialize all distances as INFINITE and stpSet[] as false
+    for (int i = 0; i < V; i++)
+        dist[i] = INT_MAX, sptSet[i] = false;
+
+    // Distance of source vertex from itself is always 0
+    dist[src] = 0;
+
+    // Find shortest path for all vertices
+    for (int count = 0; count < V - 1; count++) {
+        // Pick the minimum distance vertex from the set of vertices not
+        // yet processed. u is always equal to src in the first iteration.
+        int u = minDistance(dist, sptSet);
+
+        // Mark the picked vertex as processed
+        sptSet[u] = true;
+
+        // Update dist value of the adjacent vertices of the picked vertex.
+        for (int v = 0; v < V; v++)
+
+            // Update dist[v] only if is not in sptSet, there is an edge from
+            // u to v, and total weight of path from src to  v through u is
+            // smaller than current value of dist[v]
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
+                && dist[u] + graph[u][v] < dist[v])
+                dist[v] = dist[u] + graph[u][v];
+    }
+
+    // print the constructed distance array
+    printSolution(dist, src);
+}
+
 int main()
 {
     std::vector<int> array1 = { 3, 2, 1 };
@@ -41,6 +110,14 @@ int main()
     std::vector<int> array5 = { 0 };
 
     std::vector<std::vector<int>> arrays = { array1, array2, array3, array4, array5 };
+
+    for (int i = 0; i < (int)arrays.size(); i++) {
+        std::cout << "Array " << i + 1 << ": ";
+        for (int j = 0; j < (int)arrays[i].size(); j++) {
+            std::cout << arrays[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
 
     for (int i = 0; i < (int) arrays.size(); i++) {
         std::chrono::nanoseconds seconds = bubbleSort(arrays[i]);
@@ -54,6 +131,23 @@ int main()
         }
         std::cout << "\n";
     }
+
+    std::cout << '\n';
+
+    // Create graph represented as adjacency matrix
+    int graph[V][V] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+                        { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                        { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+                        { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+                        { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                        { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+                        { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+                        { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                        { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+
+    dijkstra(graph, 0);
+    dijkstra(graph, 6);
+    dijkstra(graph, 8);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
